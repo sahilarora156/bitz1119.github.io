ea3b8e38308f96ac628a9f99cc10b47ee0384b54
bittoo aggarwal <bitz1119@gmail.com>
2018-01-08 14:15:46 +0530

    converted http to https

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#but").on("click",function(e){
         //e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
         var html = '<font size = 4>'+post.content + "<br>" + "-"+"<strong>"+post.title+"</strong></font>";
